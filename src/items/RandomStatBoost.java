a3d7c9825d8d3ab9a3e9339406c74532d062e3c8
Jackie Jone <jackiejone23@gmail.com>
2022-04-15 22:35:39 +1200

    Completed Random Stat Boost Item

--- src/items/RandomStatBoost.java
+++ src/items/RandomStatBoost.java
@@ -2,21 +2,21 @@ package items;
 
 import main.Rarity;
 import monsters.Monster;
+import java.util.Random;
 
 /**
- * An item that increases a random stat of a Monster
+ * An item that increases a random stat of a Monster.
  *
  * @author Jackie Jone
  * @version 1.0, Apr 2022
  */
 public class RandomStatBoost extends Item {
-
     /**
-     * Constructor for RandomStatBoost
-     * 
-     * @param newName        name of the item
-     * @param newDescription description of the item
-     * @param newRarity      rarity of the description
+     * Constructor for RandomStatBoost.
+     *
+     * @param newName        name of the item.
+     * @param newDescription description of the item.
+     * @param newRarity      rarity of the description.
      */
     public RandomStatBoost(String newName, String newDescription, Rarity newRarity) {
         super(newName, newDescription, newRarity);
@@ -40,8 +40,38 @@ public class RandomStatBoost extends Item {
 
     }
 
-    // TODO: Implement this method based on item and rarity
+    /**
+     * Randomly boosts either the base health or base attack damage of a monster.
+     * The amount boosted is based on the rarity of the item.
+     *
+     * @param monster The moster to boost the stat of.
+     */
     public void use(Monster monster) {
+        // Generates a new random object with a random seed.
+        // TODO: Test if this actually does something different on seperate method calls.
+        Random random = new Random();
 
+        int statBoostAmount;
+
+        switch (getRarity()) {
+            case COMMON:
+                statBoostAmount = ItemConstants.COMMONSTATBOOST;
+                break;
+            case RARE:
+                statBoostAmount = ItemConstants.RARESTATBOOST;
+                break;
+            case LEGENDARY:
+                statBoostAmount = ItemConstants.LEGENDARYSTATBOOST;
+                break;
+            default:
+                statBoostAmount = ItemConstants.COMMONSTATBOOST;
+        }
+
+        // TODO: Turn this into an enum? change that random 3 into a constant inside generic monster?
+        if (random.nextInt(3) == 0) {
+            monster.increaseBaseHealth(statBoostAmount);
+        } else {
+            monster.increaseBaseAttackDamage(statBoostAmount);
+        }
     }
 }
