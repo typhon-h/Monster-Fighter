5502a6a8d9c19e5a6a75067d59210d2d5bc228a0
Jackie Jone <jjo134@uclive.ac.nz>
2022-04-15 16:22:17 +1200

    Added Player Class

---
+++ src/Team.java
@@ -0,0 +1,10 @@
+/**
+ * A class to hold and manage a group of monsters.
+ * Used to create a team of monsters used by Player and BattleManager.
+ * 
+ * @author Jackie Jone
+ * @version 1.0, Apr 2022.
+ */
+public class Team {
+
+}
