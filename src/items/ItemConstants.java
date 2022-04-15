b10dda3514ad9fda279e9307631cedd3d83c2846
Jackie Jone <jjo134@uclive.ac.nz>
2022-04-15 19:19:11 +1200

    Added Item constants

---
+++ src/items/ItemConstants.java
@@ -0,0 +1,60 @@
+package items;
+
+/**
+ * Class containting constants for items based on rarity level
+ *
+ * @author Jackie Jone
+ * @version 1.0, Apr 2022
+ */
+public final class ItemConstants {
+
+    // TODO: tweak these constants to balance game.
+
+    /**
+     * Constant defining the amount a common item should boost a stat
+     * of a monster
+     */
+    public static final int COMMONSTATBOOST = 1;
+
+    /**
+     * Constant defining the amount a rare item should boost a stat
+     * of a monster
+     */
+    public static final int RARESTATBOOST = 3;
+
+    /**
+     * Constant defining the amount a legendary item should boost a stat
+     * of a monster
+     */
+    public static final int LEGENDARYSTATBOOST = 5;
+
+    /**
+     * Constant defining the cost of a common item
+     */
+    public static final int COMMONBUYPRICE = 20;
+
+    /**
+     * Constant defining the cost of a common item
+     */
+    public static final int RAREBUYPRICE = 50;
+
+    /**
+     * Constant defining the cost of a common item
+     */
+    public static final int LEGENDARYBUYPRICE = 100;
+
+    /**
+     * Constant defining the cost of a common item
+     */
+    public static final int COMMONSELLPRICE = 15;
+
+    /**
+     * Constant defining the cost of a common item
+     */
+    public static final int RARESELLPRICE = 35;
+
+    /**
+     * Constant defining the cost of a common item
+     */
+    public static final int LEGENDARYSELLPRICE = 75;
+}
