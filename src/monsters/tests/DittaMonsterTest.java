477da4309ba6725cc4fdc77d11fce88e747cd90e
Harrison Tyson <hty13@uclive.ac.nz>
2022-04-17 14:53:31 +1200

    Added DittaMonster tests

---
+++ src/monsters/tests/DittaMonsterTest.java
@@ -0,0 +1,102 @@
+package monsters.tests;
+
+import monsters.*;
+
+import items.ItemConstants;
+
+import main.Team;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import exceptions.TeamStatusException;
+
+import java.util.Queue;
+
+/**
+ * Testing for Ditta Monster class.
+ * 
+ * @author Harrison Tyson
+ * @version 1.0, Apr 2022.
+ */
+public class DittaMonsterTest {
+    Monster monster;
+
+    /**
+     * Set up a monster to test methods on before each test
+     * 
+     * @throws Exception any error that occurs
+     */
+    @BeforeEach
+    public void setUp() throws Exception {
+        monster = new DittaMonster();
+    }
+
+    /**
+     * Checks stats are set correctly according to constants
+     * Covers: Constructor
+     * BuyPrice, SellPrice, Rarity not NULL
+     * AttackDamage set to BaseAttackDamage
+     * Health set to BaseHealth
+     * BuyPrice and SellPrice set based on Rarity
+     */
+    @Test
+    public void statsTest() {
+        assertNotNull(monster.getBuyPrice());
+        assertNotNull(monster.getSellPrice());
+        assertNotNull(monster.getRarity());
+        // Check base stats are set correctly
+        assertEquals(BaseStatConstants.DITTABASEATTACKDAMAGE, monster.getBaseAttackDamage());
+        assertEquals(BaseStatConstants.DITTABASEHEALTH, monster.getBaseHealth());
+        // Check buy/sell prices are set correctly
+        switch (monster.getRarity()) {
+            case COMMON:
+                assertEquals(ItemConstants.COMMONBUYPRICE, monster.getBuyPrice());
+                assertEquals(ItemConstants.COMMONSELLPRICE, monster.getSellPrice());
+                break;
+            case RARE:
+                assertEquals(ItemConstants.RAREBUYPRICE, monster.getBuyPrice());
+                assertEquals(ItemConstants.RARESELLPRICE, monster.getSellPrice());
+                break;
+            case LEGENDARY:
+                assertEquals(ItemConstants.LEGENDARYBUYPRICE, monster.getBuyPrice());
+                assertEquals(ItemConstants.LEGENDARYSELLPRICE, monster.getSellPrice());
+                break;
+        }
+    }
+
+    /**
+     * Checks ability effect activates correctly
+     * Covers: ability
+     * Valid: effect occurs. Stats match first enemy
+     * Invalid: Whole opponent team has fainted
+     */
+    @Test
+    public void abilityTest() throws TeamStatusException {
+        Team allyTeam = new Team(monster);
+        Team enemyTeam = new Team(new ClinkMonster());
+        Queue<Monster> triggeredAbilities = monster.ability(allyTeam, enemyTeam);
+        Monster firstEnemy = enemyTeam.getFirstAliveMonster();
+
+        // Check stats are copied
+        assertEquals(0, triggeredAbilities.size());
+        assertEquals(firstEnemy.getCurrentHealth(), monster.getCurrentHealth());
+        assertEquals(firstEnemy.getCurrentAttackDamage(), monster.getCurrentAttackDamage());
+
+        // Copies modified stats
+        monster.restore();
+        firstEnemy.setCurrentAttackDamage(monster.getCurrentAttackDamage() + 1);
+        firstEnemy.setCurrentHealth(monster.getCurrentHealth() + 1);
+        monster.ability(allyTeam, enemyTeam);
+        assertEquals(firstEnemy.getCurrentHealth(), monster.getCurrentHealth());
+        assertEquals(firstEnemy.getCurrentAttackDamage(), monster.getCurrentAttackDamage());
+
+        // Nothing happens when whole team fainted (no available enemy)
+        monster.restore();
+        firstEnemy.takeDamage(firstEnemy.getCurrentHealth() + 1);
+        assertEquals(0, enemyTeam.getAliveMonsters().size()); // All enemies fainted
+        monster.ability(allyTeam, enemyTeam);
+        assertEquals(monster.getBaseHealth(), monster.getCurrentHealth());
+        assertEquals(monster.getBaseAttackDamage(), monster.getCurrentAttackDamage());
+    }
+}
