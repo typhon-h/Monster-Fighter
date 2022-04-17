a205a7714a04ae67f93bd2c5b9ac0246447d867d
Harrison Tyson <hty13@uclive.ac.nz>
2022-04-17 22:36:31 +1200

    Added TeddyMonster testing

--- src/monsters/MonsterConstants.java
+++ src/monsters/MonsterConstants.java
@@ -53,34 +53,82 @@ public class MonsterConstants {
      */
     public static final int LEGENDARYSELLPRICE = 75;
 
-    // CLINK
+    /**
+     * Constant defining the base AttackDamage of Clink
+     */
     public final static int CLINKBASEATTACKDAMAGE = 2;
+    /**
+     * Constant defining the base Health of Clink
+     */
     public final static int CLINKBASEHEALTH = 2;
+    /**
+     * Constant defining the base Rarity of Clink
+     */
     public final static Rarity CLINKRARITY = Rarity.LEGENDARY;
 
-    // DITTA
+    /**
+     * Constant defining the base AttackDamage of Ditta
+     */
     public final static int DITTABASEATTACKDAMAGE = 1;
+    /**
+     * Constant defining the base of Health Ditta
+     */
     public final static int DITTABASEHEALTH = 3;
+    /**
+     * Constant defining the base Rarity of Ditta
+     */
     public final static Rarity DITTARARITY = Rarity.LEGENDARY;
 
-    // GIL
+    /**
+     * Constant defining the base AttackDamage of Gil
+     */
     public final static int GILBASEATTACKDAMAGE = 1;
+    /**
+     * Constant defining the base Health of Gil
+     */
     public final static int GILBASEHEALTH = 3;
+    /**
+     * Constant defining the base Rarity of Gil
+     */
     public final static Rarity GILRARITY = Rarity.LEGENDARY;
 
-    // JYNX
+    /**
+     * Constant defining the base AttackDamage of Jynx
+     */
     public final static int JYNXBASEATTACKDAMAGE = 2;
+    /**
+     * Constant defining the base Health of Jynx
+     */
     public final static int JYNXBASEHEALTH = 3;
+    /**
+     * Constant defining the base Rarity of Jynx
+     */
     public final static Rarity JYNXRARITY = Rarity.LEGENDARY;
 
-    // LUCIFER
+    /**
+     * Constant defining the base AttackDamage of Lucifer
+     */
     public final static int LUCIFERBASEATTACKDAMAGE = 1;
+    /**
+     * Constant defining the base Health of Lucifer
+     */
     public final static int LUCIFERBASEHEALTH = 1;
+    /**
+     * Constant defining the base Rarity of Lucifer
+     */
     public final static Rarity LUCIFERRARITY = Rarity.LEGENDARY;
 
-    // TEDDY
+    /**
+     * Constant defining the base AttackDamage of Teddy
+     */
     public final static int TEDDYBASEATTACKDAMAGE = 1;
+    /**
+     * Constant defining the base Health of Teddy
+     */
     public final static int TEDDYBASEHEALTH = 3;
+    /**
+     * Constant defining the base Rarity of Teddy
+     */
     public final static Rarity TEDDYRARITY = Rarity.LEGENDARY;
 
 }
