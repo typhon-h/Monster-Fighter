5e44bcc6d0d72482df1c1b5bdc1ccad22a4b0c47
Harrison Tyson <hty13@uclive.ac.nz>
2022-04-24 00:28:02 +1200

    Added Shop Class

---
+++ src/main/Shop.java
@@ -0,0 +1,45 @@
+package main;
+
+import java.util.ArrayList;
+
+/**
+ * Superclass for shop objects.
+ * A shop controls the transfer of entities to and from a player
+ *
+ * @author Harrison Tyson
+ * @version 1.0, Apr 2022.
+ */
+public abstract class Shop {
+    /**
+     * ArrayList of entities available to the shop.
+     */
+    protected ArrayList<Entity> content = new ArrayList<Entity>();
+
+    /**
+     * Player that is trading with the shop
+     */
+    protected Player player;
+
+    /**
+     * Constructor for shop
+     * 
+     * @param player player that is trading with the shop
+     */
+    public Shop(Player player) {
+        this.player = player;
+    }
+
+    /**
+     * Gets ArrayList of current shop contents
+     * 
+     * @return the current content of the shop
+     */
+    public ArrayList<Entity> getContent() {
+        return this.content;
+    }
+
+    /**
+     * Sets the contents of the shop
+     */
+    public abstract void setContent();
+}
