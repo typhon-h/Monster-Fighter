1684ec8d5542b61e2d96b4a4c76783a647286711
Jackie Jone <jackiejone23@gmail.com>
2022-04-23 17:38:13 +1200

    Implemented Give Rewards

--- src/main/BattleManager.java
+++ src/main/BattleManager.java
@@ -2,8 +2,6 @@ package main;
 
 import java.util.ArrayList;
 
-import main.Player;
-
 /**
  * A class for managing all the battles and functionality of battles
  * which a player can prticipate in.
@@ -15,11 +13,60 @@ public class BattleManager {
     Player allyPlayer;
     ArrayList<Player> opponents;
     Player currentOpponent = null;
-    boolean battleResult = false;
+    BattleResult battleResult = BattleResult.NULL;
 
+    /**
+     * Constructor to for BattleManager, creates a new BattleManager
+     * for a given player.
+     *
+     * @param player The player which the BattleManager is for.
+     */
     public BattleManager(Player player) {
         allyPlayer = player;
     }
 
-    public void generateOpponents(int currentDay, int maxDays, Difficulty difficulty) {}
+    /**
+     * Generates the different oponents what the player can battle against.
+     *
+     * @param currentDay The current day of the game.
+     * @param maxDays    The maximum number of days any game can last.
+     * @param difficulty The difficulty of the current game.
+     */
+    public void generateOpponents(int currentDay, int maxDays, Difficulty difficulty) {
+        /*
+         * Math.ceil(CurrentDay / AbsolouteTotalDays) * 6
+         *
+         */
+    }
+
+    /**
+     * Gets the possible opponents which the player can face against.
+     *
+     * @return A list of {@link main.Player Opponents} that the player can face against.
+     */
+    public ArrayList<Player> getOpponents() {
+        return opponents;
+    }
+
+    /**
+     * Sets the {@link main.Player opponent} which the {@link main.Player player} is battling against
+     *
+     * @param opponent The selected {@link main.Player opponent} to battle against
+     */
+    public void setOpponent(Player opponent) {
+        currentOpponent = opponent;
+        opponents.remove(opponent);
+    }
+
+    /**
+     * Distributes rewards to the player if the player has defeated the opponent.
+     */
+    public void giveRewards() {
+        if (battleResult == BattleResult.WIN && currentOpponent != null) {
+            allyPlayer.addGold(currentOpponent.getGold());
+            allyPlayer.incrementScore(currentOpponent.getScore());
+        }
+
+        battleResult = BattleResult.NULL;
+    }
 }
