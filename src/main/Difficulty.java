259c2683176e92cdd1dee165cd49b45c5898b974
Jackie Jone <jackiejone23@gmail.com>
2022-04-23 13:19:10 +1200

    Added difficulties

---
+++ src/main/Difficulty.java
@@ -0,0 +1,23 @@
+package main;
+
+/**
+ * Different difficulties of the game
+ *
+ * @author Jackie Jones
+ *
+ * @version 1.0, Apr 2022.
+ */
+public enum Difficulty {
+    /**
+     * Easy difficulty
+     */
+    EASY,
+    /**
+     * Normal difficulty, default difficulty
+     */
+    NORMAL,
+    /**
+     * Hard difficulty
+     */
+    HARD
+}
