9a04dbab5a7ed80c67b0de78f081d0553ed4b65a
Jackie Jone <jackiejone23@gmail.com>
2022-04-26 14:25:28 +1200

    Difficulty setting in MonsterTriggerConstants

--- src/main/MonsterTriggerConstants.java
+++ src/main/MonsterTriggerConstants.java
@@ -10,34 +10,84 @@ import monsters.*;
  * each monster in the game.
  *
  * @author Jackie Jone
- * @version 1.0, Apr 2022.
+ * @version 1.1, Apr 2022.
  */
 public final class MonsterTriggerConstants {
     // TODO: Tweak the triggers for each monster
+    /*
+    * When triggers for a monster are changed, also make the same changes to
+    * the test file.
+    */
+
     /**
      * A map mapping each subclass of the monster class to an array of triggers.
      */
-    private static final Map<Class<?>, Trigger[]> triggers = Map.ofEntries(
+    private static final Map<Class<?>, Trigger[][]> triggers = Map.ofEntries(
         // Optimal Triggers for Clink Monster.
-        entry(ClinkMonster.class,   new Trigger[] {Trigger.ONFAINT,
-                                                   Trigger.STARTOFBATTLE}),
+        entry(ClinkMonster.class,   new Trigger[][] {
+            {Trigger.ONFAINT,Trigger.STARTOFBATTLE}, // Easy Triggers
+            {}, // Normal Triggers
+            {}}), // Hard Triggers
         // Optimal Triggers for Ditta Monster.
-        entry(DittaMonster.class,   new Trigger[] {Trigger.ONFAINT,
-                                                   Trigger.STARTOFBATTLE,
-                                                   Trigger.ONHURT}),
+        entry(DittaMonster.class,   new Trigger[][] {
+            {Trigger.ONFAINT, Trigger.STARTOFBATTLE, Trigger.ONHURT},
+            {},
+            {}}),
         // Optimal Triggers for Gil Monster.
-        entry(GilMonster.class,     new Trigger[] {Trigger.ONFAINT,
-                                                   Trigger.STARTOFBATTLE}),
+        entry(GilMonster.class,     new Trigger[][] {
+            {Trigger.ONFAINT, Trigger.STARTOFBATTLE},
+            {},
+            {}}),
         // Optimal Triggers for Jynx Monster.
-        entry(JynxMonster.class,    new Trigger[] {Trigger.ONFAINT,
-                                                   Trigger.STARTOFBATTLE}),
+        entry(JynxMonster.class,    new Trigger[][] {
+            {Trigger.ONFAINT, Trigger.STARTOFBATTLE},
+            {},
+            {}}),
         // Optimal Triggers for Lucifer Monster.
-        entry(LuciferMonster.class, new Trigger[] {Trigger.ONFAINT,
-                                                   Trigger.STARTOFBATTLE}),
+        entry(LuciferMonster.class, new Trigger[][] {
+            {Trigger.ONFAINT, Trigger.STARTOFBATTLE},
+            {},
+            {}}),
         // Optimal Triggers for Teddy Monster.
-        entry(TeddyMonster.class,   new Trigger[] {Trigger.ONFAINT,
-                                                   Trigger.STARTOFBATTLE})
+        entry(TeddyMonster.class,   new Trigger[][] {
+            {Trigger.ONFAINT, Trigger.STARTOFBATTLE},
+            {},
+            {}})
     );
+
+    /**
+     * Concatenates two {@link main.Trigger trigger arrays} together.
+     *
+     * @param array1 First array to concatenate to.
+     * @param array2 Second array to concatenate.
+     * @return       Concatenated arrays.
+     */
+    private static Trigger[] appendArray(Trigger[] array1, Trigger[] array2) {
+        Trigger[] finalArray = new Trigger[array1.length + array2.length];
+
+        for (int i=0; i<array1.length; i++) {
+            finalArray[i] = array1[i];
+        }
+
+        for (int i=0; i<array2.length; i++) {
+            finalArray[array1.length + i] = array2[i];
+        }
+
+        return finalArray;
+    }
+
+    private static Trigger[] getTriggerDifficulties(Trigger[][] allTriggers, Difficulty difficulty) {
+        Trigger[] finalTriggers = new Trigger[0];
+        switch (difficulty) {
+            case HARD:
+                finalTriggers = appendArray(finalTriggers, allTriggers[2]);
+            case NORMAL:
+                finalTriggers = appendArray(finalTriggers, allTriggers[1]);
+            case EASY:
+                finalTriggers = appendArray(finalTriggers, allTriggers[0]);
+        }
+        return finalTriggers;
+    }
 
     /**
      * Gets the optimal triggers for a given monster.
@@ -45,8 +95,9 @@ public final class MonsterTriggerConstants {
      * @param monster The monster to get the triggers for.
      * @return        An array of {@link main.Trigger trigger enum} values.
      */
-    public static final Trigger[] getTriggers(Monster monster) {
-        return triggers.get(monster.getClass());
+    public static final Trigger[] getTriggers(Monster monster, Difficulty difficulty) {
+        Trigger[][] allTriggers = triggers.get(monster.getClass());
+        return getTriggerDifficulties(allTriggers, difficulty);
     }
 
     /**
@@ -56,9 +107,10 @@ public final class MonsterTriggerConstants {
      * @return                          An array of {@link main.Trigger trigger enum} values.
      * @throws IllegalArgumentException if the given class is not a subclass of {@link monsters.Monster monster class}.
      */
-    public static final Trigger[] getTriggers(Class<?> monsterClass) throws IllegalArgumentException {
+    public static final Trigger[] getTriggers(Class<?> monsterClass, Difficulty difficulty) throws IllegalArgumentException {
         if (Monster.class.isAssignableFrom(monsterClass) && monsterClass != Monster.class) {
-            return triggers.get(monsterClass);
+            Trigger[][] allTriggers = triggers.get(monsterClass);
+            return getTriggerDifficulties(allTriggers, difficulty);
         } else {
             throw new IllegalArgumentException("The given class is not a subclass of Monster class");
         }
