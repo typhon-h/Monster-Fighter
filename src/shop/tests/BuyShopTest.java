04efe214bb5965153012315f10527e7b3be2c046
Harrison Tyson <hty13@uclive.ac.nz>
2022-04-26 22:57:49 +1200

    Separated shop tests for readability

--- src/shop/tests/BuyShopTest.java
+++ src/shop/tests/BuyShopTest.java
@@ -49,10 +49,12 @@ public class BuyShopTest {
      */
     @RepeatedTest(10)
     public void setContentTest() {
+        // Content was refreshed
         ArrayList<Entity> previousContent = new ArrayList<>(buyShop.getContent());
         buyShop.setContent();
         assertNotEquals(previousContent, buyShop.getContent());
 
+        // Check contains 1 monster and 1 item of each rarity
         for (Rarity rarity : Rarity.values()) {
             int monstersOfRarity = 0;
             int itemsOfRarity = 0;
@@ -77,22 +79,21 @@ public class BuyShopTest {
      * @return object of given instance in shop
      */
     private Object getObjectFromShop(Class<?> objectToRetrieve) {
-        Object objectToBuy = null;
-        for (Entity object : buyShop.getContent()) {
-            if (objectToRetrieve.isInstance(object)) {
-                objectToBuy = object;
+        Object object = null;
+        for (Entity entity : buyShop.getContent()) {
+            if (objectToRetrieve.isInstance(entity)) {
+                object = entity;
                 break;
             }
         }
-        return objectToBuy;
+        return object;
     }
 
     /**
-     * Checks items can be bought
+     * Valid item purchase
      */
     @Test
-    public void itemBuyTest() {
-        // Valid buy
+    public void validItemBuyTest() {
         Item itemToBuy = (Item) getObjectFromShop(Item.class);
         assertFalse(player.getInventory().contains(itemToBuy));
 
@@ -104,49 +105,68 @@ public class BuyShopTest {
         assertEquals(startingGold - itemToBuy.getBuyPrice(), player.getGold());
         assertFalse(buyShop.getContent().contains(itemToBuy));
         assertEquals(itemToBuy.getName() + " bought successfully", message);
+    }
 
-        // Insufficient gold
-        itemToBuy = (Item) getObjectFromShop(Item.class);
+    /**
+     * Checks can't buy item with insufficient gold
+     */
+    @Test
+    public void noGoldItemBuyTest() {
+        Item itemToBuy = (Item) getObjectFromShop(Item.class);
         assertFalse(player.getInventory().contains(itemToBuy));
         assertEquals(0, player.getGold());
 
-        message = buyShop.buy(itemToBuy);
+        String message = buyShop.buy(itemToBuy);
 
         assertFalse(player.getInventory().contains(itemToBuy));
         assertTrue(buyShop.getContent().contains(itemToBuy));
         assertEquals(itemToBuy.getBuyPrice() + " more gold is required", message);
 
-        // Inventory Full
+    }
+
+    /**
+     * Checks can't buy item with full inventory
+     */
+    @Test
+    public void fullInventoryItemBuyTest() {
+        Item itemToBuy = (Item) getObjectFromShop(Item.class);
+        assertFalse(player.getInventory().contains(itemToBuy));
+
+        // Fill inventory
         while (player.getNumFreeSlots() > 0) {
             player.addItem(new AttackBoost(Rarity.COMMON));
         }
+
         player.addGold(itemToBuy.getBuyPrice());
         assertFalse(player.getInventory().contains(itemToBuy));
-        message = buyShop.buy(itemToBuy);
+
+        String message = buyShop.buy(itemToBuy);
 
         assertFalse(player.getInventory().contains(itemToBuy));
         assertTrue(buyShop.getContent().contains(itemToBuy));
         assertEquals("Sorry, you do not have enough space in your inventory", message);
-
-        // Item not in shop
-        itemToBuy = new AttackBoost(Rarity.COMMON);
-        assertFalse(buyShop.getContent().contains(itemToBuy));
-        message = buyShop.buy(itemToBuy);
-        assertFalse(player.getInventory().contains(itemToBuy));
-        assertFalse(buyShop.getContent().contains(itemToBuy));
-        assertEquals("Buy Error: item not found", message);
-
     }
 
     /**
-     * Checks monsters can be bought
-     * 
-     * @throws TeamSizeException         team is full
-     * @throws DuplicateMonsterException member is already in team
+     * Checks can't buy an item not in the shop
      */
     @Test
-    public void monsterBuyTest() throws TeamSizeException, DuplicateMonsterException {
-        // Valid buy
+    public void noItemBuyTest() {
+        Item itemToBuy = new AttackBoost(Rarity.COMMON);
+        assertFalse(buyShop.getContent().contains(itemToBuy));
+
+        String message = buyShop.buy(itemToBuy);
+
+        assertFalse(player.getInventory().contains(itemToBuy));
+        assertFalse(buyShop.getContent().contains(itemToBuy));
+        assertEquals("Buy Error: item not found", message);
+    }
+
+    /**
+     * Valid monster purchase
+     */
+    @Test
+    public void validMonsterBuyTest() {
         Monster monsterToBuy = (Monster) getObjectFromShop(Monster.class);
         assertFalse(player.getTeam().getMonsters().contains(monsterToBuy));
 
@@ -158,44 +178,75 @@ public class BuyShopTest {
         assertEquals(startingGold - monsterToBuy.getBuyPrice(), player.getGold());
         assertFalse(buyShop.getContent().contains(monsterToBuy));
         assertEquals(monsterToBuy.getName() + " bought successfully", message);
+    }
 
-        // Insufficient gold
-        monsterToBuy = (Monster) getObjectFromShop(Monster.class);
+    /**
+     * Checks can't buy monster with no gold
+     */
+    @Test
+    public void noGoldMonsterBuyTest() {
+        Monster monsterToBuy = (Monster) getObjectFromShop(Monster.class);
         assertFalse(player.getTeam().getMonsters().contains(monsterToBuy));
         assertEquals(0, player.getGold());
 
-        message = buyShop.buy(monsterToBuy);
+        String message = buyShop.buy(monsterToBuy);
 
         assertFalse(player.getTeam().getMonsters().contains(monsterToBuy));
         assertTrue(buyShop.getContent().contains(monsterToBuy));
         assertEquals(monsterToBuy.getBuyPrice() + " more gold is required", message);
+    }
 
-        // Inventory Full
+    /**
+     * Checks can't buy monster with full team
+     * 
+     * @throws TeamSizeException         tries to add more members than max team
+     *                                   size
+     * @throws DuplicateMonsterException monster already in team
+     */
+    @Test
+    public void fullInventoryMonsterBuyTest() throws TeamSizeException, DuplicateMonsterException {
+        Monster monsterToBuy = (Monster) getObjectFromShop(Monster.class);
+
+        // Fill team
         while (player.getTeam().getTeamSize() < Team.getMaxTeamSize()) {
             player.getTeam().addMonster(new ClinkMonster());
         }
+
         player.addGold(monsterToBuy.getBuyPrice());
         assertFalse(player.getTeam().getMonsters().contains(monsterToBuy));
-        message = buyShop.buy(monsterToBuy);
+        String message = buyShop.buy(monsterToBuy);
 
         assertFalse(player.getTeam().getMonsters().contains(monsterToBuy));
         assertTrue(buyShop.getContent().contains(monsterToBuy));
         assertEquals("Team is full", message);
+    }
 
-        // Duplicate Monster
-        player.getTeam().removeMonster(player.getTeam().getMonsters().get(0));
-        monsterToBuy = player.getTeam().getMonsters().get(0);
+    /**
+     * Checks can't buy monster already in team
+     */
+    @Test
+    public void duplicateMonsterBuyTest() {
+        Monster monsterToBuy = player.getTeam().getMonsters().get(0);
         buyShop.getContent().add(monsterToBuy);
-        message = buyShop.buy(monsterToBuy);
-        assertEquals("Monster is already in the team", message);
+        player.addGold(monsterToBuy.getBuyPrice());
 
-        // Monster not in shop
-        monsterToBuy = new ClinkMonster();
+        String message = buyShop.buy(monsterToBuy);
+
+        assertEquals("Monster is already in the team", message);
+    }
+
+    /**
+     * Checks can't buy monster not in shop
+     */
+    @Test
+    public void noMonsterBuyTest() {
+        Monster monsterToBuy = new ClinkMonster();
         assertFalse(buyShop.getContent().contains(monsterToBuy));
-        message = buyShop.buy(monsterToBuy);
+
+        String message = buyShop.buy(monsterToBuy);
+
         assertFalse(player.getTeam().getMonsters().contains(monsterToBuy));
         assertFalse(buyShop.getContent().contains(monsterToBuy));
         assertEquals("Buy Error: monster not found", message);
-
     }
 }
