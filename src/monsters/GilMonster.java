92921cbdfa86e77396c1dd6c18d72820dec13db0
Harrison Tyson <hty13@uclive.ac.nz>
2022-05-11 02:00:46 +1200

    Fixed Gil Ability to BEHIND instead of IN FRONT

--- src/monsters/GilMonster.java
+++ src/monsters/GilMonster.java
@@ -21,19 +21,19 @@ public class GilMonster extends Monster {
                 MonsterConstants.GILRARITY, // Rarity
                 MonsterConstants.GILBASEATTACKDAMAGE, // Base AttackDamage
                 MonsterConstants.GILBASEHEALTH, // Base Health
-                "Boosts attack of ALLY in front by 50% of my ATTACK",
+                "Boosts attack of ALLY behind by 50% of my ATTACK",
                 MonsterConstants.GILBASESPEED); // Base Speed
         this.setTrigger(Trigger.STARTOFBATTLE); // TODO decide trigger
     }
 
     @Override
     public BattleEvent ability(Team allyTeam, Team enemyTeam) {
-        // Boosts attack of ALLY in front by 50% of my ATTACK
+        // Boosts attack of ALLY behind by 50% of my ATTACK
 
         int myPosition = allyTeam.getAliveMonsters().indexOf(this);
         Monster monsterToAdjust;
-        if (myPosition > 0) {
-            monsterToAdjust = allyTeam.getAliveMonsters().get(myPosition - 1);
+        if (myPosition < allyTeam.getAliveMonsters().size() - 1) {
+            monsterToAdjust = allyTeam.getAliveMonsters().get(myPosition + 1);
             monsterToAdjust.setCurrentAttackDamage(
                     monsterToAdjust.getCurrentAttackDamage() + (this.getCurrentAttackDamage() / 2));
             return new BattleEvent(allyTeam, enemyTeam,
